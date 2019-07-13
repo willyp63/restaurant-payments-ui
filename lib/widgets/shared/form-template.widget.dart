6446eb3223dfa2d111b4bd38bccbd9d120658189
Wil Pirino <wpirino1@gmail.com>
2019-07-13 16:59:45 -0600

    created for-template widget for login/signup

---
+++ lib/widgets/shared/form-template.widget.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../theme/colors.dart';
+
+class MMSFormTemplate extends StatelessWidget {
+  final List<Widget> children;
+
+  MMSFormTemplate({this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/restaurant.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Color(0x88000000),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width / 16,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Container(
+                          color: MMSColors.babyPowder,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: MediaQuery.of(context).size.width / 16,
+                            vertical: 50,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              [
+                                Container(
+                                  width: MediaQuery.of(context).size.width * 0.25,
+                                  child: Image.asset('images/mimos_logo.png'),
+                                ),
+                              ],
+                              children,
+                            ].expand((x) => x).toList(),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/shared/index.dart
+++ lib/widgets/shared/index.dart
@@ -1,3 +1,4 @@
 export './button.widget.dart';
 export './text-field.widget.dart';
 export './full-page-spinner.widget.dart';
+export './form-template.widget.dart';
