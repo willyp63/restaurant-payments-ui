1abab156c5db9420b8676198df5a8caf656db368
Wil Pirino <wpirino1@gmail.com>
2019-07-13 15:39:24 -0600

    changes to text styles

--- lib/theme/text.dart
+++ lib/theme/text.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 
 final appTextTheme = TextTheme(
-  display4: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-  display3: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
-  display2: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
-  display1: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
   headline: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
-  title: TextStyle(fontSize: 18.0),
-  subhead: TextStyle(fontSize: 18.0),
-  body2: TextStyle(fontSize: 16.0),
-  body1: TextStyle(fontSize: 14.0),
-  caption: TextStyle(fontSize: 12.0),
+  title: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+  subhead: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+  body2: TextStyle(fontSize: 14.0),
+  body1: TextStyle(fontSize: 12.0),
   button: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
-  subtitle: TextStyle(fontSize: 10.0),
 );
